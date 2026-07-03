6787819e3781b4a02510f2a5f03dd22e68bffe62
nuramina407 <nurun407@hotmail.com>
2026-07-03 19:28:50 -0400

    first-jenkins-project

--- tests/day02/page-objects-methods.spec.js
+++ tests/day02/page-objects-methods.spec.js
@@ -1,13 +1,5 @@
 import {test} from '@playwright/test';
-test('Getting the title of the page', async ({ page }) => {
-  page.goto("https://the-internet-5chk.onrender.com/");
 
-  //await page.waitForTimeout(3000);
-
-  let actualTitle = await page.title();
-
-  console.log(actualTitle);
-});
 
 test('Getting the current url of the page', async ({ page }) =>{
 
